5eafdc7e951773e7bbdea9d9e599f434a9af70bb
John Machaca <jwmachaca@gmail.com>
2025-09-30 03:02:16 -0500

    mejorando el ejercicio

--- src/excercises/Contador.jsx
+++ src/excercises/Contador.jsx
@@ -30,7 +30,13 @@ function Contador() {
     <div className="flex flex-col items-center justify-center min-h-screen">
       <Header />
       <main className="flex-grow container mx-auto bg-gray-100 p-4 flex flex-col items-center justify-center">
-        <h1 className="text-3xl font-bold underline">Contador</h1>
+        <h2 className="text-3xl font-bold underline">Ejercicio 1</h2>
+        <p className="text-lg m-4">
+          Cree un contador que tenga 3 botones: incrementar, decrementar y
+          resetear. El contador debe mostrar el valor actual y no permitir
+          valores negativos.
+        </p>
+        <h3 className="text-3xl font-bold underline">Contador</h3>
         <p className="text-2xl">El contador actual es: {count}</p>
         <div className="m-4 flex p-4">
           <button
