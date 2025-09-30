c8b6b2d7eb9d6bec83a027044da4b3de58005173
John Machaca <jwmachaca@gmail.com>
2025-09-30 02:20:16 -0500

    agregando codigo

--- src/excercises/Contador.jsx
+++ src/excercises/Contador.jsx
@@ -2,8 +2,15 @@ import { useState } from "react";
 import BackToMenu from "../components/BackToMenu";
 import Header from "../components/Header";
 import "../App.css";
+import Prism from "prismjs";
+import "prismjs/components/prism-jsx";
+import "prismjs/themes/prism-tomorrow.css";
+import { useEffect } from "react";
 
 function Contador() {
+  useEffect(() => {
+    Prism.highlightAll();
+  }, []);
   const [count, setCount] = useState(0);
 
   const increment = () => {
@@ -45,6 +52,49 @@ function Contador() {
             Resetear
           </button>
         </div>
+        <pre>
+          <code className="language-jsx">
+            {`
+            //esto es un comentario
+            const [count, setCount] = useState(0);
+
+            const increment = () => {
+              setCount(count + 1);
+            };
+            const decrement = () => {
+              setCount(count - 1);
+              if (count <= 0) {
+                setCount(0);
+              }
+            };
+            const reset = () => {
+              setCount(0);
+            };
+            <p className="text-2xl">El contador actual es: {count}</p>
+        <div className="m-4 flex p-4">
+          <button
+            className="p-4 rounded-md bg-blue-500 text-white m-2 text-xl"
+            onClick={increment}
+          >
+            Incrementar
+          </button>
+          <button
+            className="p-4 rounded-md bg-red-500 text-white m-2 text-xl"
+            onClick={decrement}
+          >
+            Decrementar
+          </button>
+          <button
+            className="p-4 rounded-md bg-green-500 text-white m-2 text-xl"
+            onClick={reset}
+          >
+            Resetear
+          </button>
+        </div>
+            
+            `}
+          </code>
+        </pre>
       </main>
       <BackToMenu />
     </div>
